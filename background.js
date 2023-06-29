6aaf535fc6397d77ee7e7885a9f0c3933e89218f
prd-hieu-nguyen-minh <hieu.nguyen.minh@paradox.ai>
2023-06-29 15:23:33 +0700

    Set value for required field

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-      file: 'content.js',
-    });
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript({
+    file: 'content.js',
   });
-  
+});
--- content.js
+++ content.js
@@ -1,3 +1,31 @@
-document.querySelector('[data-testid="cem_input_firstname"]').value = "Hieu";
-document.querySelector('[data-testid="cem_input_lastname"]').value = "Nguyen";
-document.querySelector('[data-testid="cem_input_email"]').value = "hieu.nguyen.minh+sdsd@paradox.ai";
+var info = {
+  "first_name": "Choose",
+  "last_name": "Time",
+  "email_prefix": "hieu.nguyen.minh",
+};
+
+var changeEvent = new Event('input', { bubbles: true, cancelable: true });
+
+var map = {
+  '[data-testid="cem_input_firstname"]': info.first_name,
+  '[data-testid="cem_input_lastname"]': info.last_name + " " + makeid(2),
+  '[data-testid="cem_input_email"]': info.email_prefix + "+" + makeid(5) + "@paradox.ai",
+};
+
+function makeid(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+for (const [key, value] of Object.entries(map)) {
+  var input = document.querySelector(key);
+  input.value = value;
+  input.dispatchEvent(changeEvent);
+}
