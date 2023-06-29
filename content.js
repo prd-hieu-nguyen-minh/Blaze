b960ca29122569defa9a2f9693d74cae23fc7304
hieuminh89 <hieuminh22490@gmail.com>
2023-06-29 08:03:45 +0700

    Initial project

---
+++ content.js
@@ -0,0 +1,3 @@
+document.querySelector('[data-testid="cem_input_firstname"]').value = "Hieu";
+document.querySelector('[data-testid="cem_input_lastname"]').value = "Nguyen";
+document.querySelector('[data-testid="cem_input_email"]').value = "hieu.nguyen.minh+sdsd@paradox.ai";
