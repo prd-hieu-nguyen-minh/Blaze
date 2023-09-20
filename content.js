2e61f86fa9b7280460e515c7f4d9a6af6a8497a1
prd-hieu-nguyen-minh <hieu.nguyen.minh@paradox.ai>
2023-09-20 17:12:07 +0700

    Feat: Auto fill new Employee Info

--- content.js
+++ content.js
@@ -4,12 +4,22 @@ var info = {
   "email_prefix": "hieu.nguyen.minh",
 };
 
-var changeEvent = new Event('input', { bubbles: true, cancelable: true });
+var changeEvent = new Event('input', {
+  bubbles: true,
+  cancelable: true
+});
 
-var map = {
+var newCandidateMap = {
   '[data-testid="cem_input_firstname"]': info.first_name,
   '[data-testid="cem_input_lastname"]': info.last_name + " " + makeid(2),
   '[data-testid="cem_input_email"]': info.email_prefix + "+" + makeid(5) + "@paradox.ai",
+};
+
+var newUserMap = {
+  '[data-testid="user_detail_input_user_fname"]': info.first_name,
+  '[data-testid="user_detail_input_user_lname"]': "User" + " " + makeid(2),
+  '[data-testid="user_detail_input_job_title"]': "dev",
+  '[data-testid="user_detail_input_user_email"]': info.email_prefix + "+" + makeid(5) + "@paradox.ai",
 };
 
 function makeid(length) {
@@ -24,8 +34,17 @@ function makeid(length) {
   return result;
 }
 
-for (const [key, value] of Object.entries(map)) {
-  var input = document.querySelector(key);
-  input.value = value;
-  input.dispatchEvent(changeEvent);
+function fillData(map) {
+  for (const [key, value] of Object.entries(map)) {
+    try {
+      var input = document.querySelector(key);
+      input.value = value;
+      input.dispatchEvent(changeEvent);
+    } catch (e) {
+      continue;
+    }
+  }
 }
+
+fillData(newCandidateMap);
+fillData(newUserMap);
